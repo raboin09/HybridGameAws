3cfae58a9d18b827c5aeda8b093f7cb478ea2638
raboin09 <raboin09@gmail.com>
2021-09-28 20:10:19 -0700

    Added first stack

--- AwsGameliftService/aws_gamelift_service/aws_gamelift_service_stack.py
+++ AwsGameliftService/aws_gamelift_service/aws_gamelift_service_stack.py
@@ -1,15 +1,18 @@
 from aws_cdk import (
-    core,
-    aws_lambda,
-    aws_dynamodb,
-    aws_events,
-    aws_events_targets,
+    core, aws_dynamodb
 )
 
 
-class AwsGameliftServiceStack(cdk.Stack):
+class AwsGameliftServiceStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        matchmaking_table = aws_dynamodb
+        matchmaking_tickets_table = aws_dynamodb.Table(
+            self,
+            id="matchmaking_tickets",
+            partition_key=aws_dynamodb.Attribute(
+                name="id",
+                type=aws_dynamodb.AttributeType.STRING
+            )
+        )
