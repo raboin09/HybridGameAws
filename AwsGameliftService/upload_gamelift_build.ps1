97e147d49c0497bbad4c0715cbc6e894240d998b
raboin09 <raboin09@gmail.com>
2021-10-10 09:22:14 -0700

    Updating Aws branch

--- AwsGameliftService/upload_gamelift_build.ps1
+++ AwsGameliftService/upload_gamelift_build.ps1
@@ -1,5 +1,7 @@
-region="us-east-1"
-stage="dev"
+$region="us-east-1"
+$stage="dev"
+
+scripts\PostToSlack.bat ":zap: Starting server upload process to ``$region``..."
 
 # Returns the status of a stack
 Function Get-Status-Of-Stack {
@@ -7,13 +9,15 @@ Function Get-Status-Of-Stack {
 	aws cloudformation describe-stacks --region $region --stack-name $name --query Stacks[].StackStatus --output text 2> Out-Null
 }
 
-# Deploy the build to GameLift (Expecting that it was built from Unity already)
-Write-Host "Deploying build (Expecting it is prebuilt in HybridGameUnreal\WindowsServer folder)"
 $buildversion = Get-Date -UFormat "%y-%m-%d.%H%M%S"
-aws gamelift upload-build --operating-system AMAZON_LINUX_2 --build-root ..\HybridGameUnreal\LinuxServer --name "``$stage``-ug-hybrid-gamelift-build" --build-version $buildversion --region $region
+scripts\PostToSlack.bat ":satellite: Creating GameLift Build for ``$buildversion``..."
+
+aws gamelift upload-build --operating-system AMAZON_LINUX_2 --build-root ..\..\..\out\HybridGameUnreal\LinuxServer --name "``$stage``-ug-hybrid-gamelift-build" --build-version $buildversion --region $region
 
 # Get the build version for fleet deployment
 $query = """Builds[?Version==``$buildversion``].BuildId"""
 $buildid = aws gamelift list-builds --query $query --output text --region $region
+
+Write-Host "Found build id: ``$buildid``"
 
 cdk deploy --profile dev-admin -v -c build_id=$buildid build_version=$buildversion
